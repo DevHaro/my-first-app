06f0c495c54c04f86da14241873ff7617d3281bd
DevHaro <harojose2019@gmail.com>
2020-10-29 20:35:09 -0600

    Clase 03

---
+++ src/components/Counter/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0,
+    }
+  }
+
+  render() {
+  return <h1>Counter: {this.state.counter}</h1>
+  }
+}
+
+export default Counter
